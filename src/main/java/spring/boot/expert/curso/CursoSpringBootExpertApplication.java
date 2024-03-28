25a631db4b28d1865b62e4f941fd62deb246bd96
Douglas Fragoso <douglas.iff@gmail.com>
2024-03-27 21:50:32 -0300

    first commit

---
+++ src/main/java/spring/boot/expert/curso/CursoSpringBootExpertApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.expert.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/expert/curso/CursoSpringBootExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.expert.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
